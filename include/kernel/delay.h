9f8cfb52f558f54aea2b8bc06253b360c53ee823
Dmytro Stechenko <dmytro.stechenko@gmail.com>
2023-07-27 08:47:34 -0400

    [kernel] add delay in cycles dummy util

---
+++ include/kernel/delay.h
@@ -0,0 +1,9 @@
+// Copyright (C) Dmytro Stechenko
+// License: http://www.gnu.org/licenses/gpl.html
+
+#ifndef KERNEL_DELAY
+#define KERNEL_DELAY
+
+void delay(unsigned long cycles);
+
+#endif // !KERNEL_DELAY
