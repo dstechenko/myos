5bee793a15538d0e57575009822d987c6b706734
Dmytro Stechenko <dmytro.stechenko@gmail.com>
2023-07-31 18:07:45 -0400

    [kernel] add bool

---
+++ include/kernel/bool.h
@@ -0,0 +1,12 @@
+// Copyright (C) Dmytro Stechenko
+// License: http://www.gnu.org/licenses/gpl.html
+
+#ifndef KERNEL_BOOL_H
+#define KERNEL_BOOL_H
+
+typedef bool _Bool;
+
+#define true 1
+#define false 0
+
+#endif // !KERNEL_BOOL_H
