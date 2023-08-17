29e1511866b8592a1c7412a8d2d5909e791828d4
Dmytro Stechenko <dmytro.stechenko@gmail.com>
2023-08-16 14:46:51 -1000

    [boot] refactor

--- include/kernel/util/bits.h
+++ include/kernel/util/bits.h
@@ -4,8 +4,6 @@
 #ifndef KERNEL_UTIL_BITS_H
 #define KERNEL_UTIL_BITS_H
 
-#include <stdint.h>
-
 #define BIT(b) (1 << b)
 
 #define MASK_LOW_HALF_BYTE(b) (b & 0x0F)
@@ -16,14 +14,5 @@
 
 #define MASK_LOW_WORD(dw) (dw & 0x0000FFFF)
 #define MASK_HIGH_WORD(dw) (dw & 0xFFFF0000)
-
-#define LOW_HALF_BYTE(b) ((uint8_t)MASK_LOW_HALF_BYTE(b))
-#define HIGH_HALF_BYTE(b) ((LOW_HALF_BYTE(b >> 4)))
-
-#define LOW_BYTE(w) ((uint8_t)MASK_LOW_BYTE(w))
-#define HIGH_BYTE(w) ((LOW_BYTE(w >> 8)))
-
-#define LOW_WORD(dw) ((uint16_t)MASK_LOW_WORD(dw))
-#define HIGH_WORD(dw) ((LOW_WORD(dw >> 16)))
 
 #endif // !KERNEL_UTIL_BITS_H
