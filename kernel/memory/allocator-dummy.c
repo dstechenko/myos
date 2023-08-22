a592f0e041ff95ebe9e069a4d74d3f541f0a0886
Dmytro Stechenko <dmytro.stechenko@gmail.com>
2023-08-22 08:36:14 -1000

    [kernel] more refactoring

--- kernel/memory/allocator-dummy.c
+++ kernel/memory/allocator-dummy.c
@@ -5,21 +5,17 @@
 
 #include <stdint.h>
 
+#include <asm/page.h>
 #include <kernel/core/config.h>
 #include <kernel/memory/ops.h>
 #include <kernel/util/assert.h>
 #include <kernel/util/bool.h>
-
-#define TABLE_SIZE 512
-#define SECTION_SIZE (PAGE_SIZE + TABLE_SIZE)
 
 #define STATIC_MEMORY_MIN (100 * PAGE_SIZE)
 #define STATIC_MEMORY_HIGH 0x3F000000
 
 #define STATIC_MEMORY (STATIC_MEMORY_HIGH - STATIC_MEMORY_MIN)
 #define STATIC_PAGES (STATIC_MEMORY / PAGE_SIZE)
-
-#if CONFIG_ENABLED(CONFIG_KERNEL_ALLOCATOR_STATIC)
 
 #define PAGE_TO_MEMORY(page) ((void *)(STATIC_MEMORY_MIN + page * PAGE_SIZE))
 #define MEMORY_TO_PAGE(addr) (((uintptr_t)addr - STATIC_MEMORY_MIN) / PAGE_SIZE)
@@ -57,5 +53,3 @@ void free(const void *obj) {
   if (obj)
     memory_map[MEMORY_TO_PAGE(obj)] = false;
 }
-
-#endif // CONFIG_ENABLED(CONFIG_KERNEL_ALLOCATOR_STATIC)
