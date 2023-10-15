1968c550d1bf2b2ddfa336d6dd70e267a35922c2
Dmytro Stechenko <dmytro.stechenko@gmail.com>
2023-10-15 09:56:20 -0400

    [kernel] refactor pages/memory utilitites

--- kernel/memory/allocator-dummy.c
+++ kernel/memory/allocator-dummy.c
@@ -3,45 +3,22 @@
 
 #include <kernel/memory/allocator.h>
 
-#include <stdint.h>
-
-#include <asm/page-defs.h>
-#include <kernel/core/config.h>
 #include <kernel/memory/ops.h>
+#include <kernel/memory/page.h>
 #include <kernel/util/assert.h>
-#include <kernel/util/bool.h>
-
-#define STATIC_MEMORY_MIN (100 * PAGE_SIZE)
-#define STATIC_MEMORY_HIGH 0x3F000000
-
-#define STATIC_MEMORY (STATIC_MEMORY_HIGH - STATIC_MEMORY_MIN)
-#define STATIC_PAGES (STATIC_MEMORY / PAGE_SIZE)
-
-#define PAGE_TO_MEMORY(page) ((void *)(STATIC_MEMORY_MIN + page * PAGE_SIZE))
-#define MEMORY_TO_PAGE(addr) (((uintptr_t)addr - STATIC_MEMORY_MIN) / PAGE_SIZE)
-
-static bool memory_map[STATIC_PAGES] = {0};
 
 void *alloc(const size_t size, const uint8_t flags) {
-  size_t i;
-
   (void)flags;
   (void)size;
 
   ASSERT(size);
   ASSERT(size <= PAGE_SIZE);
 
-  for (i = 0; i < sizeof(memory_map); i++)
-    if (!memory_map[i]) {
-      memory_map[i] = true;
-      return PAGE_TO_MEMORY(i);
-    }
-
-  return NULL;
+  return (void *)get_free_page();
 }
 
 void *zalloc(const size_t size, const uint8_t flags) {
-  uint8_t *obj = (uint8_t *)alloc(size, flags);
+  void *obj = alloc(size, flags);
 
   if (obj)
     memzero(obj, size);
@@ -49,7 +26,4 @@ void *zalloc(const size_t size, const uint8_t flags) {
   return obj;
 }
 
-void free(const void *obj) {
-  if (obj)
-    memory_map[MEMORY_TO_PAGE(obj)] = false;
-}
+void free(const void *obj) { free_page((uintptr_t)obj); }
---
+++ kernel/memory/page.c
@@ -0,0 +1,52 @@
+// Copyright (C) Dmytro Stechenko
+// License: http://www.gnu.org/licenses/gpl.html
+
+#include <kernel/memory/page.h>
+
+#include <stddef.h>
+
+#include <asm/memory-defs.h>
+#include <kernel/util/bool.h>
+
+#define STATIC_MEMORY_MIN (100 * PAGE_SIZE)
+#define STATIC_MEMORY_HIGH 0x3F000000
+
+#define STATIC_MEMORY (STATIC_MEMORY_HIGH - STATIC_MEMORY_MIN)
+#define STATIC_PAGES (STATIC_MEMORY / PAGE_SIZE)
+
+#define INDEX_TO_MEMORY(page) ((uintptr_t)(STATIC_MEMORY_MIN + page * PAGE_SIZE))
+#define MEMORY_TO_INDEX(addr) ((addr - STATIC_MEMORY_MIN) / PAGE_SIZE)
+
+static bool pages[STATIC_PAGES];
+
+static uintptr_t get_virtual_address(uintptr_t paddr) { return VIRTUAL_MEMORY_START + paddr; }
+
+uintptr_t get_free_page() {
+  size_t i;
+
+  for (i = 0; i < sizeof(pages); i++)
+    if (!pages[i]) {
+      pages[i] = true;
+      return INDEX_TO_MEMORY(i);
+    }
+
+  return (uintptr_t)NULL;
+}
+
+uintptr_t get_free_page_zero() {
+  uintptr_t page = get_free_page();
+
+  if (page)
+    memzero((void *)get_virtual_address(page), PAGE_SIZE);
+
+  return page;
+}
+
+uintptr_t get_free_page_kernel() { return (uintptr_t)NULL; }
+
+uintptr_t get_free_page_user(struct task *task, uintptr_t vaddr) { return (uintptr_t)NULL; }
+
+void free_page(const uintptr_t page) {
+  if (page)
+    pages[MEMORY_TO_INDEX(page)] = false;
+}
